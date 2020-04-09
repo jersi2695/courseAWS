92a073669231224fea390475ae625fb3270bef1d
Jhonatan España <jersi2695@hotmail.com>
2020-04-09 17:05:24 -0500

    Course of AWS

--- 08-spring-boot-todo-rest-api-eb-cli/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 08-spring-boot-todo-rest-api-eb-cli/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World - Change V3";
+		return "Hello World - Change V4";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
